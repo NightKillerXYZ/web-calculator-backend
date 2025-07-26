032a3f02bea99ad143740d50fb1464fe024bd8b8
NightKillerXYZ <shoryagoyal2010@gmail.com>
2025-07-26 16:54:46 +0530

    Describe your change here, e.g., 'fix bug in backend logic'

--- backend.py
+++ backend.py
@@ -77,4 +77,6 @@ def calculate():
             return jsonify({'error': str(e), 'result': '', 'solutions': []})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
